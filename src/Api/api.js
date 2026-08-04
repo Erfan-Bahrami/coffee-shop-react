4cf0bd66c9e5901794dbc25a54f4a83a640bbba1
Erfan <erfanbahrami12@gmail.com>
2026-08-04 00:47:26 -0700

    Update API URL

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+   baseURL: "https://coffee-shop-api-ka5h.onrender.com",
     headers:{
         "Content-Type":"application/json",
     }
